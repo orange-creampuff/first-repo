d6ffec9167c27b1e4b5b465f6679fe9f93f4e014
orange-creampuff <45663677+orange-creampuff@users.noreply.github.com>
2018-12-12 10:09:26 -0500

    Working function

--- triangles.py
+++ triangles.py
@@ -1,7 +1,13 @@
-#Make a variable for each value
-Side1 = input("First side")
-Side2 = input("Second side")
+import math
 
-#Calculate the hypotenuse
-Hypotenuse = (Side1^2 + Side2^2)^0.5
+#Allow work many times
+desire = input("Do math?")
+while desire.lower() not in ["no", "n", "stop" "don't" "dont" "nah"]:
+    #Make a variable for each value
+    Side1 = int(input("First side"))
+    Side2 = int(input("Second side"))
 
+    #Calculate the hypotenuse
+    Hypotenuse = math.sqrt(Side1**2 + Side2**2)
+    print(Hypotenuse)
+    desire = input("Do math?")
