241c064fe1f6db7d73203d207069592759618569
orange-creampuff <45663677+orange-creampuff@users.noreply.github.com>
2018-12-12 09:51:13 -0500

    Update triangles.py
    Added the real function and removed pointless text

--- triangles.py
+++ triangles.py
@@ -1,1 +1,7 @@
-Make a right triangle and calculate sides.
+#Make a variable for each value
+Side1 = input("First side")
+Side2 = input("Second side")
+
+#Calculate the hypotenuse
+Hypotenuse = (Side1^2 + Side2^2)^0.5
+
